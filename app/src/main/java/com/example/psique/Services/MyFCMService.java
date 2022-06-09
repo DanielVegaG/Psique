9b71355ea311ce2337a7d0682351ec18745f80ee
Daniel Vega González <dani@codefuente.com>
2022-06-09 14:00:36 +0200

    Chat 8

---
+++ app/src/main/java/com/example/psique/Services/MyFCMService.java
@@ -0,0 +1,30 @@
+package com.example.psique.Services;
+
+
+import androidx.annotation.NonNull;
+
+import com.example.psique.Utils.Constants;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Map;
+import java.util.Random;
+
+public class MyFCMService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage message) {
+        super.onMessageReceived(message);
+
+        Map<String, String> dataRec = message.getData();
+        if(dataRec != null){
+            Constants.showNotification(this, new Random().nextInt(),
+                    dataRec.get(Constants.NOTIF_TITLE),
+                    dataRec.get(Constants.NOTIF_CONTENT),
+                    dataRec.get(Constants.NOTIF_SENDER),
+                    dataRec.get(Constants.NOTIF_ROOM_ID),
+                    null);
+        }
+
+    }
+}
